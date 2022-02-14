36df5cf8873ac50b5d41d4a6ff7b182b67731a52
Ivan <keranamv@gmail.com>
2022-02-14 11:24:13 +0100

    Initial Commit

---
+++ Seedlinks/SeedlinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedlinksApp.swift
+//  Seedlinks
+//
+//  Created by Ivan Voloshchuk on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SeedlinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
