bbe4ef72c5eb0336bf493ee70c9f58369f921d29
Rafas02 <92868940+Rafas02@users.noreply.github.com>
2022-02-14 12:22:26 +0100

    Firebase Raf

--- Seedlinks/SeedlinksApp.swift
+++ Seedlinks/SeedlinksApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SeedlinksApp: App {
+    init() { FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
