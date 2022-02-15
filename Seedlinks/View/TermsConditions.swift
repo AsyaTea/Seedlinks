49117dcddc0f84b1d37c22596208926a9eee3b6e
morbuen <morbuen@live.it>
2022-02-15 17:58:14 +0100

    TTC/Privacy

--- Seedlinks/View/TermsConditions.swift
+++ Seedlinks/View/TermsConditions.swift
@@ -10,26 +10,12 @@ import SwiftUI
 
 
 struct TTCView: View {
-    
     var body: some View {
-        
-        ScrollView{
-            VStack {
-                Text("Terms and conditions")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .padding()
-                    .frame(width: 390, alignment: .leading)
-                Text("his Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.")
-                    .font(.body)
-                    .fontWeight(.regular)
-                    .padding()
-                    .frame(width: 390, alignment: .leading)
-            }
-        }.frame(width: 400.0, height: 800.0)
-    }
+        HTMLView(htmlFileName: "TTC")
+            .frame(width: 400.0, height: 700.0)
+        }
 }
-struct ContentView_Previe: PreviewProvider {
+struct ContentView_Previ: PreviewProvider {
     static var previews: some View {
         TTCView()
     }
