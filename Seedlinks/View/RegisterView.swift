a5891c6c951ca70ac4ba7b7865fcb350bb44a8f6
morbuen <morbuen@live.it>
2022-02-16 09:33:36 +0100

    small fix

--- Seedlinks/View/RegisterView.swift
+++ Seedlinks/View/RegisterView.swift
@@ -22,118 +22,119 @@ struct RegisterView: View {
     var body: some View {
         NavigationView{
             VStack {
-            Text("Get started!")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.center)
-            Text("Use your email to create an account")
-                .font(.body)
-                .fontWeight(.regular)
-                .multilineTextAlignment(.center)
-            Divider()
-            ScrollView {
-                VStack {
-                    Text("Username")
-                        .font(.caption)
-                        .padding(.top, 30.0)
-                        .frame(width: 370, height: 10, alignment: .leading)
-                    UsernameTextField(username: $username)
-                    Text("E-mail")
-                        .font(.caption)
-                        .padding(.top, 30.0)
-                        .frame(width: 370, height: 10, alignment: .leading)
-                    emailTextField(email: $email)
-                    Text("Password")
-                        .font(.caption)
-                        .padding(.top, 30.0)
-                        .frame(width: 370, height: 10, alignment: .leading)
-                    PasswordSecureField(password: $password)
-                    if registrationDidFail {
-                        Text("You already have an account. Please, sign in instead.")
-                            .foregroundColor(.red)
-                    }
-                    
-                    Button(action: {
-                        if self.username == storedUsername && self.password == storedPassword {
-                            self.registrationDidSucceed = false
-                            self.registrationDidFail = true
-                        } else {
-                            self.registrationDidFail = false
+                Text("Get started!")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                Text("Use your email to create an account")
+                    .font(.body)
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
+                Divider()
+                ScrollView {
+                    VStack {
+                        Text("Username")
+                            .font(.caption)
+                            .padding(.top, 30.0)
+                            .frame(width: 370, height: 10, alignment: .leading)
+                        UsernameTextField(username: $username)
+                        Text("E-mail")
+                            .font(.caption)
+                            .padding(.top, 30.0)
+                            .frame(width: 370, height: 10, alignment: .leading)
+                        emailTextField(email: $email)
+                        Text("Password")
+                            .font(.caption)
+                            .padding(.top, 30.0)
+                            .frame(width: 370, height: 10, alignment: .leading)
+                        PasswordSecureField(password: $password)
+                        if registrationDidFail {
+                            Text("You already have an account. Please, sign in instead.")
+                                .foregroundColor(.red)
                         }
-                    }) {
-                        RegisterButtonContent()
+                        
+                        Button(action: {
+                            if self.username == storedUsername && self.password == storedPassword {
+                                self.registrationDidSucceed = false
+                                self.registrationDidFail = true
+                            } else {
+                                self.registrationDidFail = false
+                            }
+                        }) {
+                            RegisterButtonContent()
+                        }
                     }
-                }
-                .padding(.bottom, 3.0)
-                HStack{
-                    Rectangle()
-                        .frame(width: 50.0, height: 1.0)
-                    Text("Or continue with")
-                    Rectangle()
-                        .frame(width: 50.0, height: 1.0)
+                    .padding(.bottom, 3.0)
+                    HStack{
+                        Rectangle()
+                            .frame(width: 50.0, height: 1.0)
+                        Text("Or continue with")
+                        Rectangle()
+                            .frame(width: 50.0, height: 1.0)
+                        
+                    }
+                    SignInWithAppleButton(
+                        onRequest: { request in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        },
+                        onCompletion: { result in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        }
+                    )
+                        .frame(width: 200.0, height: 40.0)
+                        .cornerRadius(10)
+                        .padding()
+                    //            Qui da mettere quello di Google
+                    SignInWithAppleButton(
+                        onRequest: { request in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        },
+                        onCompletion: { result in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        }
+                    )
+                        .frame(width: 200.0, height: 40.0)
+                        .cornerRadius(10)
+                        .padding()
+                    //            Qui da mettere quello di Facebook
+                    SignInWithAppleButton(
+                        onRequest: { request in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        },
+                        onCompletion: { result in
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                        }
+                    )
+                        .frame(width: 200.0, height: 40.0)
+                        .cornerRadius(10)
+                        .padding()
                     
-                }
-                SignInWithAppleButton(
-                    onRequest: { request in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    },
-                    onCompletion: { result in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    HStack {
+                        Text("Already registered?")
+                        Text("Sign in now!")
+                            .foregroundColor(Color("AccentColor"))
+                        //                    .onTapGesture {
+                        //                        <#code#>
+                        //                    }
                     }
-                )
-                    .frame(width: 200.0, height: 40.0)
-                    .cornerRadius(10)
                     .padding()
-                //            Qui da mettere quello di Google
-                SignInWithAppleButton(
-                    onRequest: { request in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    },
-                    onCompletion: { result in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
+                    HStack {
+                        Text("By registering, you agree to our")
+                        
+                        NavigationLink(destination: PolicyView())
+                        {Text("privacy policy")
+                                .foregroundColor(Color("AccentColor"))
+                        }
                     }
-                )
-                    .frame(width: 200.0, height: 40.0)
-                    .cornerRadius(10)
-                    .padding()
-                //            Qui da mettere quello di Facebook
-                SignInWithAppleButton(
-                    onRequest: { request in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    },
-                    onCompletion: { result in
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Code@*/ /*@END_MENU_TOKEN@*/
-                    }
-                )
-                    .frame(width: 200.0, height: 40.0)
-                    .cornerRadius(10)
-                    .padding()
-                
-                HStack {
-                    Text("Already registered?")
-                    Text("Sign in now!")
-                    //                    .onTapGesture {
-                    //                        <#code#>
-                    //                    }
-                }
-                .padding()
-                HStack {
-                    Text("By registering, you agree to our")
-                    
-                    NavigationLink(destination: PolicyView())
-                    {Text("privacy policy")
-                            .foregroundColor(Color.green)
+                    HStack{
+                        Text("and our")
+                        NavigationLink(destination: TTCView())
+                        {Text("terms and conditions.")
+                                .foregroundColor(Color("AccentColor"))
+                        }
                     }
                 }
-                HStack{
-                    Text("and our")
-                    NavigationLink(destination: TTCView())
-                    {Text("terms and conditions.")
-                            .foregroundColor(Color.green)
-                    }
-                }
-            }
-        }.frame(width: 400.0, height: 850.0)
+            }.frame(width: 400.0, height: 850.0)
         }
         
     }
