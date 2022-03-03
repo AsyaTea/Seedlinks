e9170a51bea8e40eea36fd1237082630820c0bf5
morbuen <morbuen@live.it>
2022-03-03 10:15:31 +0100

    minor changes to localization

--- Seedlinks/View/ProfileView.swift
+++ Seedlinks/View/ProfileView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+let filterDate: String = "Date"
+let filterDistance: String = "Distance"
+
 struct ProfileView: View {
     
     @ObservedObject var userSession : UserSession
@@ -15,7 +18,7 @@ struct ProfileView: View {
     @State var isOn: Bool = false
     var username : String = ""
     @State private var selectedCategory = ""
-    var filtering = ["Date","Distance"]
+    var filtering = [NSLocalizedString(filterDate, comment: ""), NSLocalizedString(filterDistance, comment: "")]
     
     var customLabel: some View {
         HStack {
